387855e1b6d5adc0dd8091f64949fd604d4956ce
jiekechoo <jiekechoo@gmail.com>
2015-04-29 14:22:08 +0800

    增加http session 保存token到redis支持，增加 加密密码验证

---
+++ src/main/java/demo/HttpSessionConfig.java
@@ -0,0 +1,26 @@
+package demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.session.web.http.HttpSessionStrategy;
+
+
+@Configuration
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
+public class HttpSessionConfig {
+
+	@Bean
+	public JedisConnectionFactory connectionFactory() {
+		JedisConnectionFactory factory = new JedisConnectionFactory();
+		return factory;
+	}
+
+	@Bean
+	public HttpSessionStrategy httpSessionStrategy() {
+		return new HeaderHttpSessionStrategy();
+	}
+
+}
--- src/main/java/demo/SecurityConfig.java
+++ src/main/java/demo/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -21,7 +22,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			throws Exception {
 		auth
 		.jdbcAuthentication()
-		.dataSource(dataSource);
+		.dataSource(dataSource)
+		.passwordEncoder(new BCryptPasswordEncoder(10));
 	}
 
 	@Override
