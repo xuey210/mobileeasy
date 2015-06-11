634f9981c8100a89cc463d284df1791d7d6d2d23
jiekechoo <jiekechoo@gmail.com>
2015-06-11 15:40:32 +0800

    暂停redis管理session模式，测试更加方便。

--- src/main/java/demo/session/HttpSessionConfig.java
+++ src/main/java/demo/session/HttpSessionConfig.java
@@ -1,9 +1,5 @@
 package demo.session;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * 会话信息保存到redis定义，头信息保存
@@ -11,30 +7,31 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
  * @author jiekechoo
  *
  */
-@Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
-public class HttpSessionConfig {
-
-	/**
-	 * 定义Jedis连接工厂
-	 * 
-	 * @return factory
-	 */
-	@Bean
-	public JedisConnectionFactory connectionFactory() {
-		JedisConnectionFactory factory = new JedisConnectionFactory();
-		return factory;
-	}
-
-	// 暂时取消 x-auth-token，以默认cookie代替
-	// /**
-	// * HttpSessionStrategy
-	// *
-	// * @return
-	// */
-	// @Bean
-	// public HttpSessionStrategy httpSessionStrategy() {
-	// return new HeaderHttpSessionStrategy();
-	// }
-
-}
+// 暂停回话保存在redis（2015.6.11）
+//@Configuration
+//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
+//public class HttpSessionConfig {
+//
+//	/**
+//	 * 定义Jedis连接工厂
+//	 * 
+//	 * @return factory
+//	 */
+//	@Bean
+//	public JedisConnectionFactory connectionFactory() {
+//		JedisConnectionFactory factory = new JedisConnectionFactory();
+//		return factory;
+//	}
+//
+//	 暂时取消 x-auth-token，以默认cookie代替
+//	 /**
+//	 * HttpSessionStrategy
+//	 *
+//	 * @return
+//	 */
+//	 @Bean
+//	 public HttpSessionStrategy httpSessionStrategy() {
+//	 return new HeaderHttpSessionStrategy();
+//	 }
+//
+// }
